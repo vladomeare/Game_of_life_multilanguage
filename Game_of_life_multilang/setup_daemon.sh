2f893896eddf1ac824bb7a4ececd19ee7772dbf1
vladomeare <31183196+vladomeare@users.noreply.github.com>
2021-07-28 00:14:14 +0200

    Update setup_daemon.sh

--- Game_of_life_multilang/setup_daemon.sh
+++ Game_of_life_multilang/setup_daemon.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 PATH=$( pwd )
+
+cat > /etc/systemd/system/Game_of_life.service << EOF
+/[Unit]
+Description=Daemon for game of life
+/[Service]
+ExecStart= $PATH
+Restart=always
+/[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod -x /etc/systemd/system/Game_of_life.service
