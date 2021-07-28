526206e598ec03e390d626acd3eb9b42dba2dbd4
vladomeare <31183196+vladomeare@users.noreply.github.com>
2021-07-28 23:20:41 +0200

    Update game_of_life_script.sh

--- Game_of_life_multilang/script/game_of_life_script.sh
+++ Game_of_life_multilang/script/game_of_life_script.sh
@@ -2,24 +2,3 @@
 
 RUN=1
 RESTART=0
-CURRENT_DIR=$(pwd)
-
-python3 $CURRENT_DIR"/../python/generate_zerofield.py"
-echo "Field generate: 200"
-
-while (($RUN == 1))
-do
-	$CURRENT_DIR"/../Cpp/game_of_life"
-	RUN=$(python3 $CURRENT_DIR"/../python/html_builder.py")
-	sleep 3
-	echo "html-page building: 200"
-	echo "run code: $RUN"
-done
-
-if [$RUN -eq 0]
-echo "exit"
- exec $CURRENT_DIR"/game_of_life_script.sh"
- exit
-else
- echo "error in game of life!"
-fi
