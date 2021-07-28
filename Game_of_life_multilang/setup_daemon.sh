9499c9905fd0cd9a4251228f70ab9c64c5da2f0e
vladomeare <31183196+vladomeare@users.noreply.github.com>
2021-07-28 23:20:56 +0200

    Update setup_daemon.sh

--- Game_of_life_multilang/setup_daemon.sh
+++ Game_of_life_multilang/setup_daemon.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-chmod +x ./script/game_of_life_script.sh
-
 P=$( pwd )
+
+echo "CURRENT_DIR=/""$P"/"" >> ./script/game_of_life_script.sh
+
+cat >> ./scipt/game_of_life_script.sh << EOF
+
+python3 $CURRENT_DIR"/../python/generate_zerofield.py"
+echo "Field generate: 200"
+
+while (($RUN == 1))
+do
+	$CURRENT_DIR"/../Cpp/game_of_life"
+	RUN=$(python3 $CURRENT_DIR"/../python/html_builder.py")
+	sleep 3
+	echo "html-page building: 200"
+	echo "run code: $RUN"
+done
+
+if [$RUN -eq 0]
+echo "exit"
+ exec $CURRENT_DIR"/game_of_life_script.sh"
+ exit
+else
+ echo "error in game of life!"
+fi
+
+EOF
+
+chmod +x ./script/game_of_life_script.sh
 
 cat > /etc/systemd/system/Game_of_life.service << EOF
 [Unit]
