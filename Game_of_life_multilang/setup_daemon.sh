c6d069df22e37bef4196390f437de7ca1a276607
vladomeare <31183196+vladomeare@users.noreply.github.com>
2021-07-28 15:21:16 +0200

    Update setup_daemon.sh

--- Game_of_life_multilang/setup_daemon.sh
+++ Game_of_life_multilang/setup_daemon.sh
@@ -13,3 +13,6 @@ WantedBy=multi-user.target
 EOF
 
 chmod -x /etc/systemd/system/Game_of_life.service
+systemctl daemon-reload 
+systemctl enable --now /etc/systemd/system/Game_of_life.service
+systemctl status /etc/systemd/system/Game_of_life.service
